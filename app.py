10812d597c7c8944384646209e70d990f0b4ec7a
abubakarnsit <abubakar.jmi16@gmail.com>
2020-07-11 02:39:01 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,redirect,request
+from sklearn.externals import joblib
+
+
+app=Flask(__name__)
+
+
+
+model=joblib.load("harword_pays_model.pkl")
+
+@app.route('/')#routing i.e when a client will enter this url the below function will performed
+def hello():
+	return render_template("index.html")  #write html code in this index.html file saved in templates folder
+   
+
+
+@app.route('/', methods=['POST'])
+def marks():
+	if request.method=='POST':
+		hours=float(request.form['work'])
+		marks=str(model.predict([[hours]])[0][0])
+		marks=float(marks)
+		marks=round(marks,2)
+		marks=str(marks)
+	return render_template('index.html',your_marks=marks)
+
+
+
+if __name__=='__main__':
+	app.run(debug=True)
